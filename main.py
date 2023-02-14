ee737c3803ae4a9288d480f5161e3fac27d455d5
Austin <47459115+AustinChen123@users.noreply.github.com>
2023-02-14 23:35:23 +0100

    modefied the new code for discord version over 2.0

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
-
-await bot.load_extension("TranslateCog")
-
-
-bot.run(TOKEN)
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
